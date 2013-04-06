004f2cc857be3ab3bae0a1c8588a653aba678c00
Paul Himes <paul@paulhimes.com>
2013-04-06 10:48:02 -0500

    Hooked up transition to contact details page

--- scripts/contact_viewer.js
+++ scripts/contact_viewer.js
@@ -1,3 +1,6 @@
+var currentContact
+
+// Load the Contact List Page
 $("#contactListPage").on("pagebeforeshow", function() {
 	// Get all the contacts.
 	$.get('http://contacts.tinyapollo.com/contacts?key=letitbe',
@@ -6,9 +9,16 @@ $("#contactListPage").on("pagebeforeshow", function() {
 	  $('#contactList').html('')
 	  for(var i = 0; i < contacts.length; i++) {
 	    // Add each contact to the list.
-	    $('#contactList').append('<li><a href="ContactDetail.html?id=' + contacts[i]._id + '">' + contacts[i].name + '</a></li>')
+		currentContact = contacts[i]
+	    $('#contactList').append('<li><a href="#contactDetailsPage">' + contacts[i].name + '</a></li>')
 	  }
 		$('#contactList').listview('refresh')
 	},
 	'json')
+})
+
+// Load the Contact Details Page
+$("#contactDetailsPage").on("pagebeforeshow", function() {
+	// Fill in the data labels.
+	$('#contactDetailsContent').html(currentContact.name)
 })
