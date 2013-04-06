6281329cb484c7a60d7daf5c96245f8ec07ad25f
Paul Himes <paul@paulhimes.com>
2013-04-06 10:29:37 -0500

    Fixed list view styling by adding a refresh call

--- scripts/contact_viewer.js
+++ scripts/contact_viewer.js
@@ -1,10 +1,14 @@
-// Get all the contacts.
-$.get('http://contacts.tinyapollo.com/contacts?key=letitbe',
-function(data) {
-  var contacts = data.contacts
-  for(var i = 0; i < contacts.length; i++) {
-    // Add each contact to the list.
-    $('#contactList').append('<li data-role="listitem"><a href="ContactDetail.html?id=' + contacts[i]._id + '">' + contacts[i].name + '</a></li>')
-  }
-},
-'json')
+$("#contactListPage").on("pagebeforeshow", function() {
+	// Get all the contacts.
+	$.get('http://contacts.tinyapollo.com/contacts?key=letitbe',
+	function(data) {
+	  var contacts = data.contacts
+	  $('#contactList').html('')
+	  for(var i = 0; i < contacts.length; i++) {
+	    // Add each contact to the list.
+	    $('#contactList').append('<li data-role="listitem"><a href="ContactDetail.html?id=' + contacts[i]._id + '">' + contacts[i].name + '</a></li>')
+		$('#contactList').listview('refresh')
+	  }
+	},
+	'json')
+})
