1be007dd1be9ded15fb246a526aec4fbb15a82c9
Chad M. Linke <chadmlinke@comcast.net>
2013-04-09 16:19:24 -0500

    fixed contact_viewer to get the information for the correct contact rather than the last one

--- scripts/contact_viewer.js
+++ scripts/contact_viewer.js
@@ -12,23 +12,30 @@ $("#contactListPage").on("pagebeforeshow", function() {
             // Add each contact to the list.
             var name = contacts[i].name
             var id = contacts[i]._id
-            $('#contactList').append('<li id="contact' + id + '"><a href="#contactDetailsPage">' + name + '</a></li>')
-            $('#contact' + id).on("click", function() {
-              $.get('http://contacts.tinyapollo.com/contacts/' + id + '/?key=letitbe',
+
+            $('#contactList').append('<li id="contact' + id + '" accessKey="' + id + '"><a href="#contactDetailsPage">' + name + '</a></li>')     //added accessKey CML 04/09/2013
+
+            $('#contact' + id).on("click", function(event){           //added event to get the details of the sender (target) CML 04/09/2013
+
+              $.get('http://contacts.tinyapollo.com/contacts/' + event.currentTarget.accessKey + '/?key=letitbe',       //needed to get id dynamically after onCLick.  Used accessKey  CML 04/09/2013
+
                     function(data) {
-                      console.log(data.contact)
+                      //console.log(data.contact)
                       // This doesn't work because it's in the wrong scope...?
                       currentContact = data.contact
                     },
-                    'json')})
+                    'json'
+                  )})
           }
           $('#contactList').listview('refresh')
         },
         'json')
 })
 
+
 // Load the Contact Details Page
 $("#contactDetailsPage").on("pagebeforeshow", function() {
 	// Fill in the data labels.
-	$('#contactDetailsContent').html(currentContact.name)
+	         //$('#contactDetailsContent').html(currentContact.name)
+           console.log(currentContact)
 })
