ba9e98332fddca15ef3b1ef58d10a22dd4c1cfbc
Chad M. Linke <chadmlinke@comcast.net>
2013-04-10 01:14:00 -0500

    Added Edit page and Add page.  Still no functionality when clicking save.

--- scripts/contact_viewer.js
+++ scripts/contact_viewer.js
@@ -1,9 +1,12 @@
-currentContact = "DateHere"
+var currentContact = 0
 var abcde = "fghij"
 
 // Load the Contact List Page
 $("#contactListPage").on("pagebeforeshow", function() {
-	// Get all the contacts.
+	// Get all the contacts
+
+  currentContact=0;     //reset variable so when we add we don't have a prefill
+
 	$.get('http://contacts.tinyapollo.com/contacts?key=letitbe',
         function(data) {
           var contacts = data.contacts
@@ -34,6 +37,40 @@ $("#contactListPage").on("pagebeforeshow", function() {
         'json')
 })
 
+
+
+// Load the Edit Details Page
+$("#editContactPage").on("pagebeforeshow", function() {
+
+          if (currentContact==0){
+            //this is an add form
+            $('#editContactHeaderContent').html('Add Contact')
+
+          }
+          else {
+            $('#editContactHeaderContent').html('Edit Contact')
+            $('#contactName').val(currentContact.name)
+            $('#contactTitle').val(currentContact.title)
+            $('#contactEmail').val(currentContact.email)
+            $('#contactPhone').val(currentContact.phone)
+            $('#contactTwitterId').val(currentContact.twitterId)
+          }
+          //Add edit form
+          
+
+})
+
+
+
+$('#saveButton').on("click", function(){
+    //validate fields
+
+    //collect fields
+
+    //send fields to saveContact
+
+})
+
 //Used to load the contact information to the page.  Thought it made more sence than doing so in the function above.
 function loadContact(){
 
@@ -42,18 +79,29 @@ function loadContact(){
   var currentEmail = currentContact.email
   var currentPhone = currentContact.phone
   var currentTwitter = currentContact.twitterId
+    $('#contactDetailsContent').html('')
+    $('#contactDetailsContent').append('<h1>' + currentContact.name + '</h1>')
+    $('#contactDetailsContent').append('<hr><h3>' + currentTitle + '</h3>')
+    $('#contactDetailsContent').append(currentEmail + '<br>')
+    $('#contactDetailsContent').append(currentPhone + '<br>')
+    $('#contactDetailsContent').append(currentTwitter + '<br>')
+}
 
-  $('#contactDetailsContent').html('<h1>' + currentContact.name + '</h1><hr><h3>' + currentTitle + '</h3>' + currentEmail + '<br>' + currentPhone + '<br>' + currentTwitter + '<br>')
+//Save Contact to the ContactList
+function saveContact(id,name,title,email,phone,twitterId){
+    //if id = 0 it is ad add.
+
 }
 
 
 
-/*     Not Using
-// Load the Contact Details Page
-$("#contactDetailsPage").on("pagebeforeshow", function(id) {                //This function is Happening BEFORE the one above that sets currentContact
 
+
+
+
+
+// Load the Contact Details Page
+$("#contactDetailsPage").on("pagebeforeshow", function() {                //This function is Happening BEFORE the one above that sets currentContact
 	// Fill in the data labels.
 	         //$('#contactDetailsContent').html(currentContact.name)
-
 })
-*/
